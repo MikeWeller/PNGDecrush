8bcbc5847773e2f3153792c9e07a6d6973295294
Michael Weller <mike@mikeweller.com>
2013-01-30 11:16:41 +0100

    PNG crushed IDAT chunks are now fixed by recompressing with zlib

---
+++ OverTheAir/PNGDecrusher/PNGChunkParser.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace OverTheAir.PNGDecrusher
+{
+    public class PNGChunkParser
+    {
+        private static byte[] _PNGHeader = { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A };
+
+        public static IEnumerable<PNGChunk> ChunksFromStream(Stream stream)
+        {
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                if (!TryReadPNGHeaderFromReader(reader))
+                {
+                    throw new InvalidDataException("Could not find the PNG header");
+                }
+
+                List<PNGChunk> result = new List<PNGChunk>();
+                PNGChunk chunk;
+                while ((chunk = ReadChunk(reader)) != null)
+                {
+                    result.Add(chunk);
+                }
+
+                return result;
+            }
+        }
+
+        private static PNGChunk ReadChunk(BinaryReader reader)
+        {
+            if (ReaderIsAtEndOfFile(reader))
+            {
+                return null;
+            }
+
+            uint length = reader.ReadUInt32NetworkByteOrder();
+            string type = Encoding.UTF8.GetString(reader.ReadBytes(4));
+            byte[] data = reader.ReadBytes((int)length);
+            uint crc = reader.ReadUInt32NetworkByteOrder();
+
+            return new PNGChunk(type, data, crc);
+        }
+
+        private static bool ReaderIsAtEndOfFile(BinaryReader reader)
+        {
+            return reader.PeekChar() == -1;
+        }
+
+        private static bool TryReadPNGHeaderFromReader(BinaryReader reader)
+        {
+            byte[] actualHeader = reader.ReadBytes(_PNGHeader.Length);
+
+            return ((IStructuralEquatable)actualHeader).Equals(_PNGHeader, StructuralComparisons.StructuralEqualityComparer);
+        }
+
+        public static void WriteChunksWithHeader(IEnumerable<PNGChunk> chunks, Stream output)
+        {
+            using (BinaryWriter writer = new BinaryWriter(output))
+            {
+                writer.Write(_PNGHeader);
+
+                foreach (PNGChunk chunk in chunks)
+                {
+                    WriteChunkToWriter(chunk, writer);
+                }
+            }
+        }
+
+        private static void WriteChunkToWriter(PNGChunk chunk, BinaryWriter writer)
+        {
+            writer.WriteNetworkOrder((uint)chunk.Data.Length);
+            
+            byte[] typeString = Encoding.UTF8.GetBytes(chunk.TypeString);
+            if (typeString.Length != 4)
+            {
+                throw new InvalidDataException("PNG chunk type must be a 4 character string");
+            }
+
+            writer.Write(typeString);
+            writer.Write(chunk.Data);
+            writer.WriteNetworkOrder(chunk.DataCRC);
+        }
+    }
+}
--- OverTheAirTests/DecrushPNGTests.cs
+++ OverTheAirTests/DecrushPNGTests.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Collections;
 using System.Text;
 using System.IO.Compression;
+using System.Drawing;
 
 namespace OverTheAirTests
 {
@@ -24,12 +25,19 @@ namespace OverTheAirTests
             // * Image pixels are premultiplied with the alpha. 
             //
 
+            // Why lots of existing solutions are broken:
+            //
+            // * They do not reverse the precompression line filters before byte swapping.
+            //   This will cause sometimes subtle, sometimes not-so-subtle artifacts in the resulting image
+            // * They do not handle multiple IDAT chunks
+            //
+
             // So, what we could do is:
             //
             // * Remove the CgBI chunk
             // * Inflate the chunk data and deflate again with the headers
             //
-            // * Read this new PNG into .net's standard image manipulation classes
+            // * Read this new PNG into .net's standard image manipulation classes - so we don't have to decode the line filters or anything
             // * Reverse the byte swap
             // * Reverse the premultiplied alpha
             // * Done?
@@ -43,7 +51,7 @@ namespace OverTheAirTests
             // * We want to avoid having to deal with precompression row filters etc. so we just fix the zlib headers and then defer to .net
         }
 
-        byte[] singleIDAT10x10White = 
+        byte[] Simple10x10WhitePNG = 
             {
                 0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a, 0x00, 0x00, 0x00, 0x0d,
                 0x49, 0x48, 0x44, 0x52, 0x00, 0x00, 0x00, 0x0a, 0x00, 0x00, 0x00, 0x0a,
@@ -58,12 +66,25 @@ namespace OverTheAirTests
                 0xae, 0x42, 0x60, 0x82
             };
 
+        byte[] Crushed10x10White =  
+            {
+                0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a, 0x00, 0x00, 0x00, 0x04,
+                0x43, 0x67, 0x42, 0x49, 0x50, 0x00, 0x20, 0x06, 0x2c, 0xb8, 0x77, 0x66,
+                0x00, 0x00, 0x00, 0x0d, 0x49, 0x48, 0x44, 0x52, 0x00, 0x00, 0x00, 0x0a,
+                0x00, 0x00, 0x00, 0x0a, 0x08, 0x06, 0x00, 0x00, 0x00, 0x8d, 0x32, 0xcf,
+                0xbd, 0x00, 0x00, 0x00, 0x09, 0x70, 0x48, 0x59, 0x73, 0x00, 0x00, 0x0b,
+                0x13, 0x00, 0x00, 0x0b, 0x13, 0x01, 0x00, 0x9a, 0x9c, 0x18, 0x00, 0x00,
+                0x00, 0x0b, 0x49, 0x44, 0x41, 0x54, 0x63, 0xf8, 0x4f, 0x24, 0x60, 0x18,
+                0x55, 0x48, 0x5f, 0x85, 0x00, 0x31, 0xe7, 0x5b, 0x95, 0x00, 0x00, 0x00,
+                0x00, 0x49, 0x45, 0x4e, 0x44, 0xae, 0x42, 0x60, 0x82
+            };
+
         [TestMethod]
         public void TestChunksAreCorrectlyParsed()
         {
-            using (MemoryStream stream = new MemoryStream(singleIDAT10x10White))
+            using (MemoryStream stream = new MemoryStream(Simple10x10WhitePNG))
             {
-                PNGChunk[] chunks = PNGDecrusher.ChunksFromStream(stream).ToArray();
+                PNGChunk[] chunks = PNGChunkParser.ChunksFromStream(stream).ToArray();
                 Assert.AreEqual(6, chunks.Length);
 
                 PNGChunk idatChunk = chunks[4];
@@ -76,13 +97,13 @@ namespace OverTheAirTests
         [TestMethod]
         public void TestWritingChunksProducesIdenticalBytesAsInput()
         {
-            byte[] input = singleIDAT10x10White;
-            using (MemoryStream stream = new MemoryStream(singleIDAT10x10White))
+            byte[] input = Simple10x10WhitePNG;
+            using (MemoryStream stream = new MemoryStream(Simple10x10WhitePNG))
             {
-                PNGChunk[] chunks = PNGDecrusher.ChunksFromStream(stream).ToArray();
+                PNGChunk[] chunks = PNGChunkParser.ChunksFromStream(stream).ToArray();
 
                 MemoryStream output = new MemoryStream();
-                PNGDecrusher.WriteChunksWithHeader(chunks, output);
+                PNGChunkParser.WriteChunksWithHeader(chunks, output);
 
                 byte[] outputBytes = output.ToArray();
 
@@ -91,7 +112,7 @@ namespace OverTheAirTests
         }
 
         [TestMethod]
-        public void TestAppleChunkIsRemovedWhenFixing()
+        public void TestAppleChunkIsRemovedWhenDecrushing()
         {
             IEnumerable<PNGChunk> chunks = new List<PNGChunk>()
             {
@@ -105,6 +126,57 @@ namespace OverTheAirTests
             Assert.AreEqual(2, fixedChunks.Count());
             Assert.AreEqual(PNGChunk.ChunkType.IDAT, fixedChunks.First().Type);
             Assert.AreEqual(PNGChunk.ChunkType.IDAT, fixedChunks.Last().Type);
+        }
+
+
+
+        [TestMethod]
+        public void TestStrippedDeflateHeadersAreAddedBack()
+        {
+            IEnumerable<PNGChunk> chunks = PNGChunkParser.ChunksFromStream(new MemoryStream(Crushed10x10White));
+
+            IEnumerable<PNGChunk> decrushedChunks = PNGDecrusher.DecrushChunks(chunks);
+            PNGChunk decrushedIdatChunk = decrushedChunks.First(c => c.Type == PNGChunk.ChunkType.IDAT);
+
+            using (MemoryStream idatDataStream = new MemoryStream(decrushedIdatChunk.Data))
+            {
+                // basic check of the zlib header
+                // http://tools.ietf.org/html/rfc1950#page-4
+                int CMF = idatDataStream.ReadByte();
+                int FLG = idatDataStream.ReadByte();
+
+                int compressionMethod = CMF & 0x0F;
+                int compressionInfo = (CMF & 0xF0) >> 4;
+
+                int COMPRESSION_METHOD_DEFLATE = 8;
+                Assert.AreEqual(COMPRESSION_METHOD_DEFLATE, compressionMethod);
+               
+                using (DeflateStream deflateStream = new DeflateStream(idatDataStream, CompressionMode.Decompress))
+                using (MemoryStream decompressedStream = new MemoryStream())
+                {
+                    deflateStream.CopyTo(decompressedStream);
+                    Assert.AreEqual(410, decompressedStream.Length);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestDecrushedImageCanBeLoadedByDotNetImageClasses()
+        {
+            using (MemoryStream crushed = new MemoryStream(Crushed10x10White))
+            using (MemoryStream decrushed = new MemoryStream())
+            {
+                PNGDecrusher.DecrushPNGStreamToStream(crushed, decrushed);
+
+                using (MemoryStream reopenedStream = new MemoryStream(decrushed.ToArray()))
+                {
+                    Bitmap bitmap = new Bitmap(reopenedStream);
+                    Assert.AreEqual(bitmap.Size, new Size(10, 10));
+
+                    Color firstPixel = bitmap.GetPixel(0, 0);
+                    Assert.AreEqual(firstPixel, Color.FromArgb(255, 255, 255, 255));
+                }
+            }
         }
     }
 }
